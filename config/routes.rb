76f35ff84dfd5a5fe496c2a72e0220faf91a8ebc
romain ponsot <r.ponsot@gmail.com>
2020-08-04 17:04:42 +0200

    crud exercise RAILS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#home'
+
+  get "tasks", to: "tasks#index", as: :tasks
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get "new", to: "tasks#new"
+
+  post "tasks", to: "tasks#create", as: :create
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+  patch "tasks/:id", to: "tasks#update"
 end
